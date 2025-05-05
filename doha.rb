17b5891b14135d1c4651233505504770508f120f
kareem76 <kareem1976@live.com>
2025-05-05 23:16:57 +0000

    do

--- doha.rb
+++ doha.rb
@@ -10,6 +10,7 @@ Capybara.register_driver :chrome do |app|
   options.add_argument('--disable-gpu')
   options.add_argument('--no-sandbox')
   options.add_argument('--disable-dev-shm-usage')
+  Selenium::WebDriver::Chrome.driver_path = nil  # Let Selenium manager handle it
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
